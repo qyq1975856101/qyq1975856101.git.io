1933ca11d51376e7253eedecfb6c909a899bf116
wuhouhuacha <1975856101@qq.com>
2021-03-01 17:03:52 +0800

    Site updated: 2021-03-01 17:03:42

--- js/matery.js
+++ js/matery.js
@@ -186,17 +186,17 @@ function switchNightMode() {
             }, 2e3)
         }), 50
     var body = document.body;
-    if(body.classList.contains('dark')){
-    document.body.classList.remove('dark');
-    localStorage.setItem('dark','0');
-    $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
-    return;
-    } else {
-    document.body.classList.add('dark');
-    localStorage.setItem('dark','1');
-    $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
-    return;
-    }
+	if(body.classList.contains('dark')){
+	document.body.classList.remove('dark');
+	localStorage.setItem('dark','0');
+	$('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
+	return;
+	} else {
+	document.body.classList.add('dark');
+	localStorage.setItem('dark','1');
+	$('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
+	return;
+	}
 }
 
 function checkNightMode() {
